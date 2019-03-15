2d50f183f9c61f5e22eeb95a0f1317af8a4f5bf3
Angela Korra'ti <44175637+annathepiper@users.noreply.github.com>
2019-03-15 13:02:49 -0700

    Some initial files to open the project

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPTestDemo
+{
+    public class WPTestClient
+    {
+        // Service endpoint templates
+        private string wpSite = "%s://%s";
+
+        /**
+         * WPTestClient
+         * Constructor for the class.
+         * <param name="incomingHost">String containing the host address for the test site</param>
+         * <param name="incomingProtocol">String containing the protocol to use for URIs</param>
+         */
+        WPTestClient(string incomingHost, string incomingProtocol)
+        {
+            wpSite = String.Format(wpSite, incomingHost, incomingProtocol);
+        }
+    }
+}
