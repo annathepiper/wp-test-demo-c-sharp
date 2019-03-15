33d0694f34a98cab0da28b9838984fbfca1fc09a
Angela Korra'ti <annathepiper@fastmail.com>
2019-03-15 13:10:31 -0700

    Adding a bunch of initial files
    These are initial files from setting up a project in Visual Studio.

---
+++ WPTestDemo/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RestSharp;
+
+namespace WPTestDemo
+{
+    public class WPTestClient
+    {
+        // Service endpoint templates
+        private string wpSite = "%s://%s";
+
+        /**
+         * WPTestClient
+         * Constructor for the class.
+         * <param name="incomingHost">String containing the host address for the test site</param>
+         * <param name="incomingProtocol">String containing the protocol to use for URIs</param>
+         */
+        WPTestClient(string incomingHost, string incomingProtocol)
+        {
+            wpSite = String.Format(wpSite, incomingHost, incomingProtocol);
+        }
+
+
+    }
+}
